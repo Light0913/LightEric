6292fa26f3501b27e6b4e1fe8fc32433651a1c90
Light <126928997+Light0913@users.noreply.github.com>
2023-07-31 11:46:36 +0800

    Add files via upload

--- LightEric_private.h
+++ LightEric_private.h
@@ -5,19 +5,19 @@
 #define LIGHTERIC_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.1.0.37"
+#define VER_STRING	"1.2.0.37"
 #define VER_MAJOR	1
-#define VER_MINOR	1
+#define VER_MINOR	2
 #define VER_RELEASE	0
 #define VER_BUILD	37
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.1.0.37"
+#define FILE_VERSION	"1.2.0.37"
 #define FILE_DESCRIPTION	"C++×ÔÖÆ³ÌÐò"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"LightEric"
-#define PRODUCT_VERSION	"1.1.0.37"
+#define PRODUCT_VERSION	"1.2.0.37"
 
 #endif /*LIGHTERIC_PRIVATE_H*/
